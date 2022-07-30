6db48b7a30b81d1ef3364693e67f833a986fdebc
Damian Eads <deads@noreply.codeberg.org>
2022-07-30 02:08:02 +0200

    Bug Fix for Missing Values (#2)
    Ngelek,
    
    Please consider the following pull request to fix the issue that you were having with missing values not being properly imputed.
    
    Co-authored-by: Damian Eads <damian.eads@gmail.com>
    Reviewed-on: https://codeberg.org/ggelek/ML_Project/pulls/2
    Co-authored-by: Damian Eads <deads@noreply.codeberg.org>
    Co-committed-by: Damian Eads <deads@noreply.codeberg.org>

--- etl.py
+++ etl.py
@@ -74,6 +74,7 @@ class PandasTransformer:
                 uvals = df[colname].unique()
                 if isinstance(uvals[0], (np.bool_, bool)):
                     coltype = "numeric"
+                    colimpute = col[~np.isnan(col)].min()-1
                     AtoB[ii] = [len(tcols)]
                     BtoA[len(tcols)] = ii
                     cols = [np.asarray(df[colname].values, np.float64)]
@@ -131,15 +132,18 @@ class PandasTransformer:
                 raise ValueError("missing column %s not in data frame" % colname)
             col = df[colname]
             if self._coltypes[i] == "numeric":
-                data = np.asarray(col, dtype=np.float64).reshape(nrows, 1)
+                data = np.asarray(col, dtype=np.float64).reshape(nrows)
                 data[np.isnan(data)] = self._colimputes[i]
+                if np.isnan(data.min()):
+                    pp
+                data = data.reshape(nrows, 1)
                 tcols = tcols + [data]
             elif self._coltypes[i] == "categorical":
                 levels = self._levels[i]
-                numericized = col.replace(levels)
+                numericized = col.replace(levels).values
                 onehot = np.zeros((nrows, len(levels)))
                 for i in range(len(levels)):
-                    onehot[numericized == i] = 1.
+                    onehot[numericized == i, i] = 1.
                 tcols = tcols + [onehot]
         X = np.hstack(tcols)
         y = None
@@ -196,9 +200,35 @@ class PandasTransformer:
                 XX = X[:, self._AtoB[i]]
                 data = XX.argmax(axis=1).ravel()
                 result = pd.DataFrame({colname: data})
-                result.replace(invert_levels(self._levels[i]))
+                result = result.replace(invert_levels(self._levels[i]))
             results.append(result)
         ty = self.transform_predictions(y)
         results.append(ty)
         return pd.concat(results, axis=1)
     
+    def make_train_test_split(self, df, provide_index=False, n_splits=None, test_size=0.2, random_state=None):
+        """
+        Performs ``n_splits`` trials of two-way train, test partitioning of
+        a data frame using stratified sampling. If ``n_splits`` is not provided,
+        just 
+        """
+        from sklearn.model_selection import StratifiedShuffleSplit
+        nrows, ncols = df.shape
+        y = self.transform_target(df)
+        # we don't need any features for label-based stratified sampling. So create dummy data here. The number of features is not relevant to generating indices, so 2 columns are generated.
+        X = np.random.rand(nrows, 2)
+
+        # If ``n_splits`` is not provided, assume that there is only one trial.
+        if n_splits is None:
+            sss = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
+            train_index, test_index = next(sss.split(X, y))
+            if provide_index:
+                return train_index, test_index
+            else:
+                return df.iloc[train_index], df.iloc[test_index]
+        else:
+            sss = StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=random_state)
+            if provide_index:
+                return [(train_index, test_index) for train_index, test_index in sss.split(X, y)]
+            else:
+                return ((df.iloc[train_index], df.iloc[test_index]) for train_index, test_index in sss.split(X, y))
