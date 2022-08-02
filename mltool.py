002ddc62db969ed3d61043fe6d1d0b1ea889e7f6
Ngelek <ggelek@icloud.com>
2022-08-02 10:31:43 -0600

    made changes to mltool.py

--- mltool.py
+++ mltool.py
@@ -21,7 +21,7 @@ class Model:
 
 
 
-    def learn(self,XTest, yTest, target = None, kind = 'RF', *args, **kwargs):
+    def learn(self,df, target = None, kind = 'RF', *args, **kwargs):
         """
         1.Make sure there is a value for target, if there isnt then throw an error 
         2. Then check if the classifier is a Random Forest and if it isnt then check if the 
@@ -41,7 +41,11 @@ class Model:
         
 
         # now fit the model
-        model.fit(XTest,yTest)
+        transformer = etl.PandasTransformer()
+        transformer.fit(df, target = target)
+        X,y = transformer.transform(df)
+        
+        model.fit(X,y)
      
         self._model = model
         self._target = target
@@ -60,27 +64,21 @@ class Model:
         
         
         if self._model is None:
-            raise ValueError("You must train a model first with None first.")
+            raise ValueError("You must train a model first with learn  first.")
         else:
            p_value =pd.DataFrame({self._target:self._model.predict(df.drop(self._target,axis = 1))})
 
         return p_value
 
-    def accuracy(self, df):
+    def accuracy(self, train_X, train_y, test_X, test_y):
         """
         so I score both the training and testing sets of data and then print out what the values would be
         """
-        train_score = self._model.score(self._Xtrain,self._ytrain)
-        test_score = self._model.score(self._Xtest,self._ytest)
-        return "Train Score: ", train_score, "\nTest Score: ", test_score
+        train_score = self._model.score(train_X,train_y)
+        test_score = self._model.score(test_X,test_y)
+        return "Train Score: ", train_score, "Test Score: ", test_score
     
-    def auc(self,df):
-        """
-        using the sci kit learn shown below i found the roc_auc_score
-        https://scikit-learn.org/stable/modules/generated/sklearn.metrics.roc_auc_score.html
-        """
-        return roc_auc_score(self._ytest, RFC.predict_proba(self.Xtest)[:,1])
-
+'''
     def crossValidate(self,df,splits):
         """
         first import libraries 
@@ -105,48 +103,22 @@ class Model:
         cv_scores = pd.DataFrame({"Cross-Validation Scores":scores_rf})
 
         return cv_scores
-            
-        
+'''       
 
     def feature_importances(self,df, num_trials):
-        X = df.drop(self._target, axis = 1)
-        y = df.iloc[:,self._target]
-        r_multi = permutation_importance(self._model, X, y,
+        transformer = etl.PandasTransformer()
+        transformer.fit(df, target = target)
+        train_df, test_df = transformer.make_train_test_split(df)
+        test_X, test_y = transformer.transform(test_df)
+
+        
+        
+        r_multi = permutation_importance(self._model, test_X,test_y,
                                          n_repeats = num_trials,
                                          random_state = 0)
-        importance_score =pd.DataFrame({"Feature Importances":r_multi["importances_mean"]})
-                                      # self._model.feature_names)
-
-        return importance_score
         
-   # def grid_search(df,target, *args, **kwargs):
-            
-    def readData(self, loc):
-        #function that has a dataset url/file location/ etc as a parameter
-        import pandas as pd #import pandas
-        df = pd.read_csv(loc) #read the url given
-        return df #return dataframe
-
-"""
-    def split(X, y, size, rand, shuf):
-       
-        splits a dataset in a training and testing, needs the X and Y variables, size of testing sample, what the random state should be and if shuffle is True or false
-       
-        from sklearn.model_selection import train_test_split
-    import numpy as np
-    # import numpy for .shape()
-
-    X_train , X_test, y_train, y_test = train_test_split(X, y, test_size = size, random_state = rand, shuffle = shuf)
-    #spliting the data in testing and training
-
-      #return print("Training Set: ",np.shape(X_train), np.shape(y_train),"Testing set: ",np.shape(X_test), np.shape(y_test))#printing the training and testing shape
-    return X_train, y_train, X_test, y_test
-
-
-def get_score(model, X_train, X_test, y_train, y_test):
-    model.fit(X_train, y_train)
-    return model.score(X_test, y_test)
-"""
+        importance_score= transformer.transform_feature_importances(df,r_multi)
+        return importance_score
 
     
     
